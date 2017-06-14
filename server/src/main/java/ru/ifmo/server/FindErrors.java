79e5a8740a741a66dc375e40a906e4c3d5f29724
ZoomUp <andreymmf@gmail.com>
2017-06-14 18:42:14 +0300

    result fix

--- server/src/main/java/ru/ifmo/server/FindErrors.java
+++ server/src/main/java/ru/ifmo/server/FindErrors.java
@@ -8,6 +8,8 @@ import java.util.Map;
 public class FindErrors {
     public static String findErrorPage(int code, ServerConfig serverConfig) {
         Map<Integer, String> errors = serverConfig.getErrorPages();
+        if (errors == null)
+            return null;
 
         if (errors.containsKey(code))
             return errors.get(code);
