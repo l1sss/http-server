d68f5f51025a9d68316111c95351afae2c7365a4
ZoomUp <andreymmf@gmail.com>
2017-06-14 15:18:23 +0300

    result

---
+++ server/src/main/java/ru/ifmo/server/FindErrors.java
@@ -0,0 +1,17 @@
+package ru.ifmo.server;
+
+import java.util.Map;
+
+/**
+ * Created by Andrew on 14.06.2017.
+ */
+public class FindErrors {
+    public static String findErrorPage(int code, ServerConfig serverConfig) {
+        Map<Integer, String> errors = serverConfig.getErrorPages();
+
+        if (errors.containsKey(code))
+            return errors.get(code);
+
+        return null;
+    }
+}
